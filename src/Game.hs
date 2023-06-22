395551ad5695f72c4c6d31f90748190aabfc2192
zhzh <white.three@outlook.com>
2023-06-23 00:03:45 +0800

    更新

--- src/Game.hs
+++ src/Game.hs
@@ -1,5 +1,17 @@
 module Game (game1) where
 import System.Random
+
+-- 自定义类型
+type ChessBoard = [String]
+
+-- 全局常量
+abc :: [(Int,Int,Int)]
+abc = [(4,9,2),(3,5,7),(8,1,6),(4,3,8),(9,5,1),(2,7,6),(4,5,6),(2,5,8)]   -- 胜利条件
+
+ms :: [Int]   -- 三阶幻方
+ms = [4,9,2,3,5,7,8,1,6]  
+
+-- 游戏主程序
 game1 :: IO () 
 game1 = do 
     putStrLn "选择游戏模式：\n1. 人机对战\n2. 人人对战"
@@ -24,118 +36,137 @@ game1 = do
             putStr "输入错误，请重新"
             game1
     
+-- 人人对战模式
 p2p :: ChessBoard -> IO ()
 p2p boardList = do 
     c1 <- check boardList
-    if c1
-    then return ()
+    if c1 /= " "
+    then putStrLn (result c1)
     else do
         putStr "先手棋子“O” "
         f <- getChess boardList
         let boardList2 = getBoardList boardList ("f",f)  -- 每次落子都要更新棋盘
         board boardList2
         c2 <- check boardList2
-        if c2
-        then return ()
+        if c2 /= " "
+        then putStrLn (result c2)
         else do
             putStr "后手棋子“X” "
             s <- getChess boardList2
             let boardList3 = getBoardList boardList2 ("s",s) 
             board boardList3
             c3 <- check boardList3
-            if c3
-            then return ()
+            if c3 /= " "
+            then putStrLn (result c3)
             else p2p boardList3
 
+-- 人机对战模式
 p2e :: ChessBoard -> String -> IO ()
 p2e boardList playerChess = do
     c1 <- check boardList
-    if c1
-    then return ()
+    if c1 /= " "
+    then putStrLn (p2e_result c1 playerChess)
     else do
         f <- if playerChess == "O" then getChess boardList else robChess boardList "O"
         let boardList2 = getBoardList boardList ("f",f)
         c2 <- check boardList2
         board boardList2
-        if c2
-        then return ()
+        if c2 /= " "
+        then putStrLn (p2e_result c2 playerChess)
         else do
             s <- if playerChess == "O" then robChess boardList2 "X" else getChess boardList2
             let boardList3 = getBoardList boardList2 ("s",s)     
             board boardList3    
             c3 <- check boardList3        
-            if c3        
-            then return ()        
+            if c3 /= " "     
+            then putStrLn (p2e_result c3 playerChess)      
             else p2e boardList3 playerChess        
-        
 
+-- 输出棋局结果
+result :: String -> String 
+result winner = case winner of
+        "O" -> "先手胜利" 
+        "X" -> "后手获胜" 
+        _   -> "两位棋手下成了平局"
+
+-- 输出人机对战模式的棋局结果 ， 依赖result
+p2e_result :: String -> String -> String
+p2e_result winner player = case winner of
+        "O" -> if player == "O" then  "恭喜你" else  "很遗憾"
+        "X" -> if player == "X" then  "恭喜你" else  "很遗憾"
+        _   -> ""
+        ++ rs
+        where 
+            rs = result winner
+
+-- 获得机器的移动步骤，仔细想一下，或许不需要IO，但getChess是IO Int，两者需要具有相同类型
+-- 如果rob改成ChessBoard -> String -> Int ,那么getChess也应该包装成 ChessBoard -> Int 
 robChess :: ChessBoard -> String -> IO Int
 robChess bl pc = do 
     let (flag,chess) = winMove bl pc -- (Bool,Int)
-    if flag && rightMove bl chess
+    if flag 
     then return chess
     else do
         let (flag2,chess2) = loseMove bl pc
-        if flag2 && rightMove bl chess2
+        if flag2
         then return chess2
         else do
             let(flag3,chess3) = inCorner bl
-            if flag3 && rightMove bl chess3
+            if flag3 
             then return chess3
             else do 
                 let (flag4,chess4) = inSpace bl
-                if flag4 && rightMove bl chess4
+                if flag4 
                 then return chess4
                 else do 
                     let (flag5,chess5) = inEdge bl
-                    if flag5 && rightMove bl chess
+                    if flag5 
                     then return chess5
                     else return 0
 
+-- 用一个非IO类型的函数把getChess包装一下
+-- 试了一下，发现不能这样做
+-- humanChess :: ChessBoard -> Int
+-- humanChess bl = (getChess bl) >>= \h 
+
 winMove :: ChessBoard -> String -> (Bool,Int)
-winMove bl pc = wrssh
+winMove bl pc
+    | null w = (False,0)
+    | otherwise = head w
     where
-        abc = [(4,9,2),(3,5,7),(8,1,6),(4,3,8),(9,5,1),(2,7,6),(4,5,6),(2,5,8)]
-        nb = zip bl [4,9,2,3,5,7,8,1,6]
-        fsn = part nb
-        nn = if pc == "O" then fst fsn else snd fsn
-        rs = [[ (n,1::Int) | n <- nn ,n == a || n == b || n== c]  | (a,b,c) <- abc]
-        w = map (\s -> unzip s ) rs
-        wrs = map (\(s,x) -> if sum x == 2 then (True,15 - sum s) else (False,0)) w
-        wrss = filter (\(b,_) -> b) wrs
-        wrssr = filter (\(_,x) -> rightMove bl x) wrss
-        wrssh = if null wrssr then (False,0) else head wrssr
+        (first,second) = part (zip bl ms)
+        w = filter 
+            (\(b,x) -> b && rightMove bl x) 
+            (map 
+                (\(s,x) -> if sum x == 2 then (True,15 - sum s) else (False,0)) 
+                (map 
+                    (\s -> unzip s ) 
+                    [[ (n,1::Int) | 
+                        n <- (if pc == "O" 
+                              then first 
+                              else second) ,n == a || n == b || n== c]  |
+                             (a,b,c) <- abc]))
+
+
 
 loseMove :: ChessBoard -> String -> (Bool,Int)
 loseMove bl pc = if pc == "O" then winMove bl "X" else winMove bl "O"
 
 inCorner :: ChessBoard -> (Bool,Int)
-inCorner bl = inCor bl ms  
-    where 
-        inCor :: ChessBoard -> [Int] -> (Bool,Int)
-        inCor [] _ = (False,0)
-        inCor _ [] = (False,0)
-        inCor (b:bs) (m:mss) = if b == "1" || b == "3" || b == "7" || b == "9"  then (True,m) else inCor bs mss
-        ms = [4,9,2,3,5,7,8,1,6]
+inCorner bl = in_f bl ms (\x -> x == "1" || x == "3" || x == "7" || x == "9") 
 
 inSpace :: ChessBoard -> (Bool,Int)
-inSpace bl = inSpa  bl ms
-    where 
-        inSpa :: ChessBoard -> [Int] -> (Bool,Int)
-        inSpa [] _ = (False,0)
-        inSpa _ [] = (False,0)
-        inSpa (b:bs) (m:mss) = if b == "5" then (True,m) else inSpa bs mss
-        ms = [4,9,2,3,5,7,8,1,6]
+inSpace bl = in_f  bl ms (\x -> x == "5")
 
 inEdge :: ChessBoard -> (Bool,Int)
-inEdge bl = inEdg bl ms
-    where 
-        inEdg :: ChessBoard -> [Int] -> (Bool,Int)
-        inEdg [] _ = (False,0)
-        inEdg _ [] = (False,0)
-        inEdg (b:bs) (m:mss) = if b == "2" || b == "4" || b == "6" || b == "8" then (True,m) else inEdg bs mss
-        ms = [4,9,2,3,5,7,8,1,6]
-    -- board boardList
+inEdge bl = in_f bl ms (\x -> x == "2" || x == "4" || x == "6" || x == "8")
+
+in_f :: ChessBoard -> [Int] -> (String -> Bool) -> (Bool,Int)
+in_f [] _ _ = (False,0)
+in_f _ [] _ = (False,0)
+in_f (b:bs) (m:mss) condition 
+    | condition b = (True,m) 
+    | otherwise = in_f bs mss condition
 
 rightMove :: ChessBoard -> Int -> Bool
 rightMove bl c = if (sum r::Int) >= 1 then False else True
@@ -143,12 +174,13 @@ rightMove bl c = if (sum r::Int) >= 1 then False else True
         r = [ 1| (x,y) <- z , y == c && (x == "O" || x == "X") ]
         z = zip bl [4,9,2,3,5,7,8,1,6]
 
-check :: ChessBoard -> IO Bool
+check :: ChessBoard -> IO String
+-- 其实应该用枚举？也许重新写一遍会好一些。
 check bl
-    | win fn = putStrLn "先手获胜" >> return True
-    | win sn = putStrLn "后手获胜" >> return True
-    | draw bl = putStrLn "和棋" >> return True
-    | otherwise = return False
+    | win fn = return "O"   -- 表示先手获胜
+    | win sn = return "X"   -- 表示后手获胜
+    | draw bl = return "E"      -- 表示和棋
+    | otherwise = return " "                      -- 表示棋局继续走子
     where
         nb = zip bl [4,9,2,3,5,7,8,1,6]
         (fn,sn) = part nb
@@ -160,7 +192,7 @@ win :: [Int] -> Bool
 win ns = if (sum w :: Int) == 15 then True else False
     where
         rs = [[ n | n <- ns ,n == a || n == b || n== c]  | (a,b,c) <- abc]
-        abc = [(4,9,2),(3,5,7),(8,1,6),(4,3,8),(9,5,1),(2,7,6),(4,5,6),(2,5,8)]
+        -- abc = [(4,9,2),(3,5,7),(8,1,6),(4,3,8),(9,5,1),(2,7,6),(4,5,6),(2,5,8)]
         w = map (\s -> if sum s == 15 then 15 else 0) rs
 
 part :: [(String,Int)] -> ([Int],[Int])
@@ -184,7 +216,6 @@ putChess (b,n) (p,c)
     | p == "s" && n == c = "X"
     | otherwise = b
 
-type ChessBoard = [String]
 board :: ChessBoard -> IO ()
 board list = do
     putStrLn "+-----+-----+-----+"
